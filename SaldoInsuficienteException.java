b60235e3964a765029d4d1f4859622ef7a142542
Ysis Gomes <ysisbgomes@gmail.com>
2026-08-19 18:56:26 -0300

    Add files via upload

---
+++ SaldoInsuficienteException.java
@@ -0,0 +1,8 @@
+package excecao;
+
+public class SaldoInsuficienteException extends RuntimeException {
+
+    public SaldoInsuficienteException(String mensagem) {
+        super(mensagem);
+    }
+}
